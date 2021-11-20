36575c299d8550ae194bf0d58151a88acdaebf6d
Igor Polyakov <web@polyakovin.ru>
2021-11-21 00:38:43 +0300

    phrases updated

--- src/App/phrases.js
+++ src/App/phrases.js
@@ -1,6 +1,7 @@
 export const testTwoLetters = 'вавававааваавааваавававав';
 export const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-export const alphabetEasy = 'авгеилмнопрстуфхцчшыэю';
-export const alphabetHard = 'бдёжзйкщъья'; // пт?
-export const easyPhrases = 'Привет, Филипп! Мне тут хорошо. И вам? Это мыло просто чума! Целую!';
+export const alphabetEasy = 'абвгежилмнопрстуфхцчшыэю';
+export const alphabetHard = 'дёзйкщъья';
+export const similar1 = 'орнрорнрорнрорнорнорноноро';
+export const easyPhrases = 'Привет, Филипп! Мне тут хорошо. И вам же? Это мыло просто чума и бомба! Целую!';
 export const forZoya = 'Привет, Зоя! Как дела? Чё делаешь?';
