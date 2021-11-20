fe08585be9bb9f674361cb4d2fdbc15821c9c5a8
Igor Polyakov <web@polyakovin.ru>
2021-11-21 00:37:19 +0300

    zh improved

--- src/utils/dactyl-gestures/russian/Ж.js
+++ src/utils/dactyl-gestures/russian/Ж.js
@@ -8,9 +8,9 @@ gesture.addCurl(Finger.Middle, FingerCurl.HalfCurl);
 gesture.addCurl(Finger.Ring, FingerCurl.HalfCurl);
 gesture.addCurl(Finger.Pinky, FingerCurl.HalfCurl);
 gesture.addDirection(Finger.Thumb, FingerDirection.DiagonalUpRight);
-gesture.addDirection(Finger.Index, FingerDirection.VerticalUp);
-gesture.addDirection(Finger.Middle, FingerDirection.VerticalUp);
-gesture.addDirection(Finger.Ring, FingerDirection.VerticalUp);
-gesture.addDirection(Finger.Pinky, FingerDirection.VerticalUp);
+gesture.addDirection(Finger.Index, FingerDirection.HorizontalRight);
+gesture.addDirection(Finger.Middle, FingerDirection.HorizontalRight);
+gesture.addDirection(Finger.Ring, FingerDirection.HorizontalRight);
+gesture.addDirection(Finger.Pinky, FingerDirection.HorizontalRight);
 
 export default gesture;
