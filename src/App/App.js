37913d1404b22a805e860df0e323aaabab986567
Igor Polyakov <web@polyakovin.ru>
2021-11-22 00:41:45 +0300

    phrase choice implemented

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { easyPhrases } from './phrases';
+import * as phrases from './phrases';
 import Trainer from './Trainer';
 import './App.css';
 
@@ -15,14 +15,22 @@ const settingsToShow = [
 ];
 
 export default function App() {
-  const [phrase, setPhrase] = useState(easyPhrases);
   const [mode, setMode] = useState('menu');
   const localSettings = JSON.parse(localStorage.getItem('settings') || 'null');
   const defaultSettings = {
     isHandSkeletonShown: false,
     areHintsShown: true,
+    phrase: 'easyPhrases',
   };
   const [settings, setSettings] = useState(localSettings || defaultSettings);
+  const [phrase, setPhrase] = useState(phrases[settings.phrase]);
+
+  const updateSettings = (code, value) => {
+    const updatedSettings = {...settings};
+    updatedSettings[code] = value;
+    localStorage.setItem('settings', JSON.stringify(updatedSettings));
+    setSettings(updatedSettings);
+  };
 
   let page = null;
   switch (mode) {
@@ -33,15 +41,19 @@ export default function App() {
             key={index}
             type="checkbox"
             checked={settings[code]}
-            onChange={() => {
-              const updatedSettings = {...settings};
-              updatedSettings[code] = !updatedSettings[code];
-              localStorage.setItem('settings', JSON.stringify(updatedSettings));
-              setSettings(updatedSettings);
-            }}
+            onChange={() => updateSettings(code, !settings[code])}
           />
           {title}
         </label>)}
+        <label>
+          Упражнение:&nbsp;
+          <select value={settings.phrase} onChange={({ target }) => {
+            updateSettings('phrase', target.value);
+            setPhrase(phrases[target.value]);
+          }}>
+            {Object.keys(phrases).map(phraseKey => <option value={phraseKey}>{phraseKey}</option>)}
+          </select>
+        </label>
         <button onClick={() => setMode('training')}>Начать тренировку</button>
       </div>;
       break;
